c01b074ce232e397159f7584799132b34987b7c8
ZENO <saintwilight7@gmail.com>
2026-03-13 04:38:40 -0400

    add try/catch error handling

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ console.log(walletInfo("0xef2d443n", 200));
 console.log(checkBalance(200));
 console.log(checkBalance(50));
 console.log(checkBalance(0));
+
+async function getBalance(address) {
+	try {
+		if (!address) throw new Error ("No wallet address provided!");
+		console.log(`Fetching balance for ${address} ...`);
+		console.log("Balance: 9999 CUSD");
+	} catch (error) {
+		console.log("Error:", error.message);
+	}
+}
+
+getBalance("0xrjfr9ef23d");
+getBalance("");
